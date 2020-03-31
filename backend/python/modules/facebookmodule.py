b67cc566775ed50ad30df0d060ca1f9f34363715
stianinstebo <51327081+stianinstebo@users.noreply.github.com>
2020-03-31 12:49:11 +0200

    Update facebookmodule.py

--- backend/python/modules/facebookmodule.py
+++ backend/python/modules/facebookmodule.py
@@ -16,13 +16,13 @@ class FacebookSDKInit:
 
     def runtimeSDK(self):
 <<<<<<< HEAD
-		my_app_id = '2617980854913762'
-		my_app_secret = '1c20f679fafb3b03ce162a4a886220ae'
-		my_access_token = 'EAAlNCiriyuIBAHoQZA7WeeIrXkdGgHYD1xqNdoHAVcp1HVqSYQcRZATNySXDvF2ZCfaEkqbzaFfz9C975LagD7VyAMqfyZBAnU5qjnOe8xmOB3fe6liJ9p875BlQotLZADO49pyMP14RQfmMBOAykWrGSwgKLk2UbpHQUbNByGoZAMU6srurmqKn6d2N4RRHcZD'
+		my_app_id = ''
+		my_app_secret = ''
+		my_access_token = ''
 =======
-		my_app_id = '381492199151679'
-		my_app_secret = '475514e79fe8ce0bcd33d7668b7fbe0f'
-		my_access_token = 'EAAFa9xMD2D8BACEBnw92nqErI4BZBxSYalWGeBAM9feWFlXDRg6YeX8qJiVnJ79YonV5ZBGokHk01dbJuQba0FjBDtum5omkK37OcJPcbO2CHOTCIDhBbbHjJXVxZCPav0oyCECMKQeSnCRLYYIe3I8z8BccZB9Yq6dipRAUewZDZD'
+		my_app_id = ''
+		my_app_secret = ''
+		my_access_token = ''
 >>>>>>> d050fcacf2240d7cd453e7c1bbe98685fd589609
 
 		return FacebookAdsApi.init(my_app_id, my_app_secret, my_access_token)
